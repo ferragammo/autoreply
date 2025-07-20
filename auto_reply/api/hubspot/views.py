6842bea434066b60180e3799dec52cdf7f594eb8
Tim Kht <timosha.khvesyuk@gmail.com>
2025-07-20 03:38:00 +0300

    initial commit

--- auto_reply/api/hubspot/views.py
+++ auto_reply/api/hubspot/views.py
@@ -5,11 +5,11 @@ from auto_reply.api.hubspot.utils import get_ticket_content
 
 
 @hubspot_router.post("/ticket")
-async def add_transcription(
+async def process_ticket(
     request: Request,
 ):
     payload = await request.json()
-    print(payload)
+    print(f'ticket: {payload}')
     ticket_id = payload[0].get('objectId')
     content = await get_ticket_content(ticket_id)
-    print(content)
+    print(f'content: {content}')
