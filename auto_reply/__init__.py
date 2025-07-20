e891127759f0792167b814c45cf3c54bc6f58c47
Tim Kht <timosha.khvesyuk@gmail.com>
2025-07-20 03:09:07 +0300

    initial commit

--- auto_reply/__init__.py
+++ auto_reply/__init__.py
@@ -5,6 +5,9 @@ from fastapi import FastAPI, Request, HTTPException
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    from auto_reply.api.hubspot import hubspot_router
+    app.include_router(hubspot_router, tags=["hubspot"])
+
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
